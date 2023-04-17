49f1c22b19e5e8a060c9e98752183ad688819286
worming <worming@hotmail.be>
2023-04-17 14:08:48 +0200

    implement delete and update

--- sonarcloud/resource_project_long_lived_branch.go
+++ sonarcloud/resource_project_long_lived_branch.go
@@ -93,8 +93,29 @@ func (r resourceProjectLongLivedBranch) Create(ctx context.Context, req tfsdk.Cr
 }
 
 // Delete implements tfsdk.Resource
-func (resourceProjectLongLivedBranch) Delete(context.Context, tfsdk.DeleteResourceRequest, *tfsdk.DeleteResourceResponse) {
-	panic("unimplemented")
+func (r resourceProjectLongLivedBranch) Delete(ctx context.Context, req tfsdk.DeleteResourceRequest, resp *tfsdk.DeleteResourceResponse) {
+	var state ProjectLongLivedBranch
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	request := settings.ResetRequest{
+		Component: state.ProjectKey.Value,
+		Keys:      LONGLIVEDBRANCHREGEX,
+	}
+
+	err := r.p.client.Settings.Reset(request)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Could not reset long lived branch pattern",
+			fmt.Sprintf("The Reset request returned an error %+v", err),
+		)
+		return
+	}
+
+	resp.State.RemoveResource(ctx)
 }
 
 // Read implements tfsdk.Resource
@@ -103,6 +124,39 @@ func (resourceProjectLongLivedBranch) Read(context.Context, tfsdk.ReadResourceRe
 }
 
 // Update implements tfsdk.Resource
-func (resourceProjectLongLivedBranch) Update(context.Context, tfsdk.UpdateResourceRequest, *tfsdk.UpdateResourceResponse) {
-	panic("unimplemented")
+func (r resourceProjectLongLivedBranch) Update(ctx context.Context, req tfsdk.UpdateResourceRequest, resp *tfsdk.UpdateResourceResponse) {
+	if r.p.SetDiagErrorIfNotInitialiazed(resp.Diagnostics) {
+		return
+	}
+
+	var plan ProjectLongLivedBranch
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	request := settings.SetRequest{
+		Component: plan.ProjectKey.Value,
+		Key:       LONGLIVEDBRANCHREGEX,
+		Value:     plan.Value.Value,
+	}
+
+	err := r.p.client.Settings.Set(request)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Could not set long lived branch pattern",
+			fmt.Sprintf("The Set request returned an error %+v", err),
+		)
+	}
+
+	result := ProjectLongLivedBranch{
+		ID:         types.String{Value: plan.ID.Value},
+		ProjectKey: types.String{Value: plan.ProjectKey.Value},
+		Value:      types.String{Value: plan.Value.Value},
+	}
+
+	diags = resp.State.Set(ctx, result)
+
+	resp.Diagnostics.Append(diags...)
 }
